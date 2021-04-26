996a23b86a9f4a0bb6f468790c5d3c6d021a2889
Robert Starmer <rstarmer@gmail.com>
2021-04-26 10:29:52 -0700

    Update to fix grafana startup error

--- helm-prometheus/setup_grafana.sh
+++ helm-prometheus/setup_grafana.sh
@@ -1,3 +1,3 @@
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
-helm install grafana/grafana --namespace monitoring grafana
+helm install grafana grafana/grafana --namespace monitoring
